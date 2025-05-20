d481219128e467d260d88c08e619573b8899880c
Vahid Sadri <vsdriuc@gmail.com>
2025-05-20 10:29:29 +0330

    add projects page

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,9 +1,39 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+
+projects = [
+    {
+        "name": "habit tracker",
+        "thumb": "img/habit-tracking.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "",
+    },
+    {
+        "name": "personal finance",
+        "thumb": "img/personal-finance.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["react", "javascript"],
+        "slug": "habit-tracking",
+        "prod": "",
+    },
+    {
+        "name": "rest-api-docs",
+        "thumb": "img/rest-api-docs.png",
+        "hero": "img/habit-tracking-hero.png",
+        "categories": ["python", "web"],
+        "slug": "habit-tracking",
+        "prod": "",
+    }
+]
+
+
+
 @app.route("/")
 def home():
-    return render_template("home.html")
+    return render_template("home.html", projects=projects)
 
 
 @app.route("/about")
