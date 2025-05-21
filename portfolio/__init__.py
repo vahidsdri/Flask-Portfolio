d64dce3c23ac9e5341099679b4af95f5f6fb0386
Vahid Sadri <vsdriuc@gmail.com>
2025-05-21 11:03:57 +0330

    add 404 error handler and page

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -53,9 +53,9 @@ def project(slug):
     else:
         return render_template(f"project_{slug}.html", project=slug_to_project[slug])
 
-
-
-
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template("404.html"), 404
 
 
 
